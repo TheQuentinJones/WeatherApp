d06797c1993eb46adebd70fad458009e05155ef0
NEO <qj82189@gmail.com>
2023-08-04 09:28:21 -0400

    refining the styling in css need to work on localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var apiKey = "cea924180544dde5b612be105dafb515";
 var searchButton = document.getElementById("search-button")
-var infoEl = document.getElementById("current-weather")
 var inputEl = document.getElementById("city-search")
 var cities = [];
 
@@ -23,35 +22,36 @@ function displayWeather(lat, lon, nameCity, thisState, thisCountry) {
       console.log(veryNewData)
 
       const cityEl = document.getElementById("current-weather")
-      const infoEl = document.getElementById("weather-cards")
-
-      infoEl.innerHTML = ""
-
+      
+      const titleCityItem = document.createElement("h4")
+      titleCityItem.setAttribute("class" , "is-size-4")
       const tempItem = document.createElement("p")
       const windItem = document.createElement("p")
       const iconItem = document.createElement("img")
       const feelsLike = document.createElement("p")
 
-      cityEl.textContent = nameCity + ", " + thisState + ", " + thisCountry + " (" + dayjs.unix(veryNewData.current.dt).format("MM/DD/YYYY") + ")"
+      titleCityItem.textContent = nameCity + ", " + thisState + ", " + thisCountry + " (" + dayjs.unix(veryNewData.current.dt).format("MM/DD/YYYY") + ")"
 
       tempItem.textContent = "Temp: " + veryNewData.current.temp + " F"
       windItem.textContent = "Wind Speed: " + veryNewData.current.wind_speed + " MPH"
       iconItem.setAttribute("src", "https://openweathermap.org/img/wn/" + veryNewData.current.weather[0].icon + "@2x.png")
       feelsLike.textContent = "Feels Like: " + veryNewData.current.feels_like + " F"
 
-      infoEl.append(tempItem)
-      infoEl.append(iconItem)
-      infoEl.append(windItem)
-      infoEl.append(feelsLike)
+      cityEl.append(titleCityItem)
+      cityEl.append(tempItem)
+      cityEl.append(iconItem)
+      cityEl.append(windItem)
+      cityEl.append(feelsLike)
 
       var cardsEl = document.getElementById("weather-cards")
+      cardsEl.setAttribute( "class", "is-flex is-flex-direction-row is-flex-wrap-wrap is-justify-content-space-between mt-6")
       cardsEl.innerHTML = ""
 
 
       for (var i = 0; i < 7; i++) {
 
         var cardEl = document.createElement("div")
-        cardEl.setAttribute("class", "card")
+        cardEl.setAttribute("class", "card p-3 mt-2")
         var cardTitleEl = document.createElement("p")
         var cardPtag = document.createElement("p")
         var cardPtag2 = document.createElement("p")
@@ -98,7 +98,7 @@ function weatherApi() {
 
             console.log(data)
 
-            cities.push(data[0].name)
+            cities.push( data[0].name )
             var cityArray = JSON.stringify(cities)
             localStorage.setItem( "cities" , cityArray)
 
